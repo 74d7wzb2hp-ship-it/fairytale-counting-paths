ff7a6a499dde846040713bc05d5eb9ea39e81378
Thomas Theilig <thomas.theilig@ktp-architekten.de>
2026-09-10 15:46:23 +0200

    Update Fairytale Counting Paths (2026-09-10 15:46)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789035282|352066395';
+const CACHE_VERSION = '1789047098|606818043';
 /** @type {string} */
-const CACHE_PREFIX = 'Neon Math Run-sw-cache-';
+const CACHE_PREFIX = 'Maerchen-Zaehlpf-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
